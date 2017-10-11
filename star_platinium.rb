836b202ed9305dbcbd92da2659501b57d65296c3
hide <sam.hide.ettahiri@gmail.com>
2017-10-11 20:07:19 +0200

    Final Commit

--- star_platinium.rb
+++ star_platinium.rb
@@ -16,9 +16,7 @@ prompt = TTY::Prompt.new
 agent = Mechanize.new
 
 markdown = Steps::GetTemplate.new(prompt).call
-# url = Steps::GetUrl.new(prompt).call
-url = 'https://capsens.githost.io'
-puts "byepass get URL FOR DEV"
+url = Steps::GetUrl.new(prompt).call
 page = Steps::Signin.new(prompt, agent, url).call
 path = Steps::GetGroup.new(prompt, agent, url).call
 dunno = Steps::SetTemplate.new(agent, url, path, markdown).call
